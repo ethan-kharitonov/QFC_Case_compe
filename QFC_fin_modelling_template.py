3af5c8dbfda579eefae9507c7cbf4d367f3e8a29
Ethan kharitonov <ethan.kharitonov@gmail.com>
2025-01-19 10:54:39 -0500

    Fixed some bugs

--- QFC_fin_modelling_template.py
+++ QFC_fin_modelling_template.py
@@ -12,15 +12,18 @@ class Market:
 class Portfollio:
     def __init__(self) -> None:
         self.shares = {"HydroCorp": 0, "BrightFuture": 0}
-        self.cash = 1
+        self.cash = 100000
 
     def evaluate(self, curMarket: Market) -> float:
-        valueA = self.numSharesA * curMarket.stockPriceA
-        valueB = self.numSharesB * curMarket.stockPriceB
+        valueA = self.shares["HydroCorp"] * curMarket.stocks["HydroCorp"]
+        valueB = self.shares["BrightFuture"] * curMarket.stocks["BrightFuture"]
 
         return valueA + valueB + self.cash
 
     def sell(self, stock_TSLA: str, sharesToSell: float, curMarket: Market) -> None:
+        if sharesToSell <= 0:
+            raise ValueError("Number of shares must be positive")
+
         if sharesToSell > self.shares[stock_TSLA]:
             raise ValueError("Attempted to sell more stock than is available")
 
@@ -28,6 +31,9 @@ class Portfollio:
         self.cash += (1 - Market.transaction_fee) * sharesToSell * curMarket.stocks[stock_TSLA]
 
     def buy(self, stock_TSLA: str, sharesToBuy: float, curMarket: Market) -> None:
+        if sharesToBuy <= 0:
+            raise ValueError("Number of shares must be positive")
+        
         cost = (1 + Market.transaction_fee) * sharesToBuy * curMarket.stocks[stock_TSLA]
         if cost > self.cash:
             raise ValueError("Attempted to spend more cash than available")
@@ -39,21 +45,24 @@ class Context:
     def __init__(self) -> None:
         pass
 
-    #PUT WHATEVEAR YOU WANT HERE
+    # PUT WHATEVER YOU WANT HERE
 
-def update_portfollio(curMarket: Market, curPortfollio: Portfollio, context: Context) -> Portfollio:
-    #CODE HERE
+def update_portfollio(curMarket: Market, curPortfollio: Portfollio, context: Context):
+    # YOUR TRADING STRATEGY GOES HERE
+    curPortfollio.buy("BrightFuture", 1, curMarket)
+    curPortfollio.sell("BrightFuture", 1, curMarket)
     pass
+    
 
 ###SIMULATION###
 market = Market()
 portfollio = Portfollio()
 context = Context()
 
-for i in range(1000):
-    portfollio = update_portfollio(market, portfollio, context)
+for i in range(365):
+    update_portfollio(market, portfollio, context)
     market.updateMarket()
 
-print(portfollio.evaluate())
+print(portfollio.evaluate(market))
 
 
