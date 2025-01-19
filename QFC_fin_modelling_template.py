f10766463e2622f9afbb4232120e039e63dcf598
Ethan kharitonov <ethan.kharitonov@gmail.com>
2025-01-19 00:06:19 -0500

    Added 0.5% transaction fee

--- QFC_fin_modelling_template.py
+++ QFC_fin_modelling_template.py
@@ -1,14 +1,17 @@
+
 class Market:
+    transaction_fee = 0.005
     def __init__(self) -> None:
-        self.stocks = {"PeePee": 123, "PooPoo": 456}
+        self.stocks = {"HydroCorp": 123, "BrightFuture": 456}
 
     def updateMarket(self):
-        #Will be implemented during grading
+        #Will be implemented during grading. 
+        #This function will update the stock values to their "real" values each day.
         pass
  
 class Portfollio:
     def __init__(self) -> None:
-        self.shares = {"PeePee": 0, "PooPoo": 0}
+        self.shares = {"HydroCorp": 0, "BrightFuture": 0}
         self.cash = 1
 
     def evaluate(self, curMarket: Market) -> float:
@@ -19,13 +22,13 @@ class Portfollio:
 
     def sell(self, stock_TSLA: str, sharesToSell: float, curMarket: Market) -> None:
         if sharesToSell > self.shares[stock_TSLA]:
-            raise ValueError("Attempted to sell more stock that is available")
+            raise ValueError("Attempted to sell more stock than is available")
 
         self.shares[stock_TSLA] -= sharesToSell
-        self.cash += sharesToSell * curMarket.stocks[stock_TSLA]
+        self.cash += (1 - Market.transaction_fee) * sharesToSell * curMarket.stocks[stock_TSLA]
 
     def buy(self, stock_TSLA: str, sharesToBuy: float, curMarket: Market) -> None:
-        cost = sharesToBuy * curMarket.stocks[stock_TSLA]
+        cost = (1 + Market.transaction_fee) * sharesToBuy * curMarket.stocks[stock_TSLA]
         if cost > self.cash:
             raise ValueError("Attempted to spend more cash than available")
 
